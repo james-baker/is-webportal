511b9aecef4d4d347cac6028b88a826d367ddfa8
James Baker <james-baker@users.noreply.github.com>
2020-11-21 16:31:43 -0800

    write better hello-world

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,12 @@ import React from "react"
 import Layout from "../components/layout"
 import GlobalCSS from "../styles/global.module.css"
 
-export default function Home() {
-  return <Layout>
-      <div style={{"height": "33vh"}}></div>
-      <div className={GlobalCSS.centerChildren}>Hello world!</div>
-    </Layout>
-}
+export default () => (
+  <Layout>
+    <div style={{ height: "33vh" }}></div>
+    <div className={GlobalCSS.centerChildren}>
+      <div>It starts with intention and awareness.</div>
+      <div>Coming soon...</div>
+    </div>
+  </Layout>
+)
