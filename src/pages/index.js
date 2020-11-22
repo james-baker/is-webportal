735ddef27efb62259a826fd05a4b4141a83a91b6
James Baker <james-baker@users.noreply.github.com>
2020-11-21 16:12:16 -0800

    add global css module for util styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 import React from "react"
 import Layout from "../components/layout"
+import GlobalCSS from "../styles/global.module.css"
 
 export default function Home() {
   return <Layout>
-    <div>Hello world!</div>
+    <div className={GlobalCSS.centerChildren}>Hello world!</div>
     </Layout>
 }
