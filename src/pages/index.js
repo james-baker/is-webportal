b30106c472e855e856a80a1e103961a0b2a10423
James Baker <james-baker@users.noreply.github.com>
2020-11-21 16:23:52 -0800

    I think "vh" (view height CSS units) is usable these days

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import GlobalCSS from "../styles/global.module.css"
 
 export default function Home() {
   return <Layout>
-    <div className={GlobalCSS.centerChildren}>Hello world!</div>
+      <div style={{"height": "33vh"}}></div>
+      <div className={GlobalCSS.centerChildren}>Hello world!</div>
     </Layout>
 }
