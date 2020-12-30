95ccaa8d95b300927d68eb3ce08396b669ad3d5a
James Baker <james-baker@users.noreply.github.com>
2020-12-29 21:26:17 -0800

    Add 1/10 info session, factor out events-list component

---
+++ src/components/events-list.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default () => (
+  <div>
+    <h3>Events</h3>
+    <ul>
+      <li><b>NEW</b> Info Session January 10th! <a href="https://us02web.zoom.us/meeting/register/tZAtdOmprj4vHNa-k3wYjYN1yOK5Ylitgz1W">RSVP Here.</a></li>
+      <li><div style={{textDecoration: "line-through"}}>Info Session December 27th</div></li>
+    </ul>
+  </div>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import React from "react"
 import { graphql } from "gatsby"
-import { Link } from "gatsby"
 import GlobalCSS from "../styles/global.module.css"
 import Layout from "../components/layout"
 import ButtondownSignup from "../components/buttondownsignup"
+import EventsList from "../components/events-list"
 
 export default ({ data }) => (
   <Layout>
@@ -12,8 +12,8 @@ export default ({ data }) => (
       style={{ minWidth: "300px", maxWidth: "760px", padding: "20px" }}>
       <div dangerouslySetInnerHTML={{__html: data.pitch.childMarkdownRemark.html}}></div>
       <div className={GlobalCSS.centerMe} style={{ marginTop: "40px", marginBottom: "40px", minWidth: "300px", maxWidth: "500px"}}><ButtondownSignup/></div>
-      <div><b>NEW</b> Info Session December 27th! <a href="https://us02web.zoom.us/meeting/register/tZAqcuiprzgoH9Gs5bbMETFkZkmVd_DzpEL3">RSVP Here.</a></div>
-      <div style={{ marginTop: "100px"}}>Written by <Link to="https://www.bakejam.com">James</Link>, December 2020.</div>
+      <EventsList/>
+      <div style={{ marginTop: "100px"}}>Written by <a href="https://www.bakejam.com">James</a>, December 2020.</div>
     </div>
   </Layout>
 )
