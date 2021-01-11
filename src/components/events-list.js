0649a04b51cbcd5bd5bdeeed71e0dff333f14fbc
James Baker <james-baker@users.noreply.github.com>
2021-01-10 16:48:28 -0800

    events for next week

--- src/components/events-list.js
+++ src/components/events-list.js
@@ -2,10 +2,21 @@ import React from "react"
 
 export default () => (
   <div>
-    <h3>Events</h3>
+    <h3>Upcoming Events</h3>
     <ul>
-      <li><b>NEW</b> Info Session January 10th! <a href="https://us02web.zoom.us/meeting/register/tZAtdOmprj4vHNa-k3wYjYN1yOK5Ylitgz1W">RSVP Here.</a></li>
-      <li><div style={{textDecoration: "line-through"}}>Info Session December 27th</div></li>
+      <li>January 17th, 1pm Pacific - Informational Session for newcomers</li>
+        <ul>
+          <li><a href="https://us02web.zoom.us/meeting/register/tZ0tc-mtpjotGNNrQEzK41XdHnIYCxd10GCI">Agenda and registration</a></li>
+        </ul>
+      <li>January 17th, 2pm Pacific - General Session for all involved participants</li>
+      <ul>
+        <li><a href="https://us02web.zoom.us/meeting/register/tZwkcuqqpjMsH9ZAsWQUBkqCbOQ-6mGbFC9q">Agenda and registration</a></li>
+      </ul>
+    </ul>
+    <h3>Past Events</h3>
+    <ul>
+      <li><div style={{textDecoration: "line-through"}}>Informational Session January 10th</div></li>
+      <li><div style={{textDecoration: "line-through"}}>Informational Session December 27th</div></li>
     </ul>
   </div>
 )
