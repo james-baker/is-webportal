3f875f74c818eaba82bf8b7bb19201725da145f7
James Baker <james-baker@users.noreply.github.com>
2021-01-18 22:50:47 -0800

    Update events-list.js
    remove old events

--- src/components/events-list.js
+++ src/components/events-list.js
@@ -5,17 +5,20 @@ export default () => (
   <div>
     <h3>Upcoming Events</h3>
     <ul>
-      <li>January 17th, 1pm Pacific - Informational Session for newcomers</li>
+      <li>TBD January 24th</li>
+{/*       <li>January 17th, 1pm Pacific - Informational Session for newcomers</li>
         <ul>
           <li><a href="https://us02web.zoom.us/meeting/register/tZ0tc-mtpjotGNNrQEzK41XdHnIYCxd10GCI">Agenda and registration</a></li>
         </ul>
       <li>January 17th, 2pm Pacific - General Session for all involved participants</li>
       <ul>
         <li><a href="https://us02web.zoom.us/meeting/register/tZwkcuqqpjMsH9ZAsWQUBkqCbOQ-6mGbFC9q">Agenda and registration</a></li>
-      </ul>
+      </ul> */}
     </ul>
     <h3>Past Timeline</h3>
     <ul>
+      <li><div>2021-01-17 General Session</div></li>
+      <li><div>2021-01-17 Informational Session</div></li>
       <li><div>2021-01-10 Informational Session</div></li>
       <li><div>2020-12-27 Informational Session</div></li>
       <li><div>2020-12-19 <Link to="thecall/">Call</Link> text revised</div></li>
