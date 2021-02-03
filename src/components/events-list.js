c1c28f7ab8bacc4f01a5dd6f4ded37d5a8fe298b
James Baker <james-baker@users.noreply.github.com>
2021-02-02 19:35:59 -0800

    Update events-list.js

--- src/components/events-list.js
+++ src/components/events-list.js
@@ -5,17 +5,19 @@ export default () => (
   <div>
     <h3>Upcoming Events</h3>
     <ul>
-      <li>January 31st, 11:00-11:55am Pacific (2pm Eastern) - Informational Session for newcomers</li>
+      <li>February 7th, 11:00-11:55am Pacific (2pm Eastern) - Informational Session for newcomers</li>
       <ul>
-        <li><a href="https://us02web.zoom.us/meeting/register/tZUvcO6prDsiHNLQomdPqodaNTSgou4kzYaW">Agenda and open registration</a></li>
+        <li><a href="https://us02web.zoom.us/meeting/register/tZAtf--oqzkvGNEx3mI9jh9EecGiCWWNqjRS">Agenda and open registration</a></li>
       </ul>
-      <li>January 31st, 1:00-2:30pm Pacific (4pm Eastern) - Relational Practices Session</li>
+      <li>February 7th, 1:00-2:30pm Pacific (4pm Eastern) - Relational Practices Session</li>
       <ul>
         <li>Introductory session is prerequisite (same day is okay). Meeting link distributed internally.</li>
       </ul>
     </ul>
     <h3>Past Timeline</h3>
     <ul>
+      <li><div>2021-01-31 Relational Practices (General) Session</div></li>
+      <li><div>2021-01-31 Informational Session</div></li>
       <li><div>2021-01-24 General Session</div></li>
       <li><div>2021-01-24 Informational Session</div></li>
       <li><div>2021-01-17 General Session</div></li>
