ec59de9461f68b902bfc6d393f17dc912cd63e3b
James Baker <james-baker@users.noreply.github.com>
2021-02-21 21:16:02 -0800

    Update events-list.js

--- src/components/events-list.js
+++ src/components/events-list.js
@@ -5,11 +5,11 @@ export default () => (
   <div>
     <h3>Informational Calls</h3>
     <p>These are introductory sessions for newcomers who are curious about Intentional Society. All 
-      are welcome, and will hear a 20-minute explanatory effort on the design and current status of 
-      the community. Using a video camera and microphone is encouraged.
+      are welcome, and will hear a 25-minute explanatory effort on the design and current status of 
+      the community. Interactive Q&amp;A follows. Using a video camera and microphone is encouraged.
     </p>
-    <p><a href="https://us02web.zoom.us/meeting/register/tZ0pfu6rqjwsG9Fl8uoVJD2fgCIwlWYz4dlt">Click 
-      here to register</a> for the next call on <b>Sunday Feb 21st, 11:00-11:55am Pacific (2pm Eastern)</b>.
+    <p><a href="https://us02web.zoom.us/meeting/register/tZAtcu6orzMvHdTzZXlw2RhZHYEI0kZo-xdo">Click 
+      here to register</a> for the next call on <b>Sunday Feb 28st, 11:00-11:55am Pacific Standard Time (2pm Eastern start)</b>.
     </p>
 
     <h3>General Sessions</h3>
@@ -17,14 +17,15 @@ export default () => (
       Our primary focus is exploring a variety of relational practices towards connection, trust, and authenticity. 
       We're also spending some time each week on self-organization, decision-making, and governance topics.
       Attending an informational call is a prerequesite - and yes, doing both in one day is okay.
-      General sessions currently happen on Sundays, 1:00-2:45pm Pacific (4pm Eastern). This form might 
+      General sessions currently happen on Sundays, 1:00-2:45pm Pacific Standard Time (4pm Eastern). This form might 
       split or significantly change at any time.
     </p>
 
     <h3>Historical Timeline</h3>
     <ul>
+      <li><div>2021-02-21 General Session #6 - Empathy Circling</div></li>
+      <li><div>2021-02-21 Informational Session</div></li>
       <li><div>2021-02-14 General Session #5 - DEI</div></li>
-      <li><div>2021-02-14 Informational Session</div></li>
       <li><div>2021-02-07 General Session #4 - examining avoidance</div></li>
       <li><div>2021-02-07 Informational Session</div></li>
       <li><div>2021-01-31 General Session #3 - I seem, we seem</div></li>
