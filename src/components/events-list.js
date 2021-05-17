d4130a18129557387fbc1cb95284770081f9a767
James Baker <james-baker@users.noreply.github.com>
2021-05-16 22:42:57 -0700

    Update events-list.js

--- src/components/events-list.js
+++ src/components/events-list.js
@@ -9,22 +9,32 @@ export default () => (
       the community. Interactive Q&amp;A follows. Using a video camera and microphone is encouraged.
     </p>
     <p>These currently happen most Saturdays.&nbsp;
-      <a href="https://us02web.zoom.us/meeting/register/tZYkdemqqzkoH9RbVVCQynK8_wViMWPdiRPG">Click 
+      <a href="https://us02web.zoom.us/meeting/register/tZIvd-GpqzIiHNTy8jUibOHaPF9L70EphdVU">Click 
       here to register</a> for the next call on <b>Saturday May 15th, 11:00-11:55am Pacific Daylight Time (2pm Eastern, 6pm UTC)</b>.
     </p>
 
     <h2>General Sessions</h2>
     <p>These meetings are for members (loosely self-identified, at present) of Intentional Society. 
-      In Q1 we explored relational practices for connection and authenticity as one group together. 
-      As we grow into Q2 we're shifting to a small-group emphasis, and are in experimentation mode as we try it out.
+      We meet together as a large group, then split into small groups - some of which are multi-week voyages. 
+      These self-selected small groups explore self-developmental and relational practices. 
+      (In Q1 we explored relational practices all together to form a base of connection and authenticity.) 
       General sessions currently happen on Sundays, 1:00-2:45pm Pacific Daylight Time (4pm Eastern, 8pm UTC).
       Attending an informational call is a prerequesite.
     </p>
 
+    <h2>Meta Meetings</h2>
+    <p>This is a members-only volunteer working group providing operational and governance support. 
+      These currently happen on Mondays, 9am Pacific Daylight Time (Noon Eastern, 4pm UTC).
+    </p>
+
     <h2>Historical Timeline</h2>
     <ul>
+      <li><div>2021-05-16 General Session 2.6 - Voyaging 3/5?</div></li>
+      <li><div>2021-05-15 Informational Session</div></li>
+      <li><div>2021-05-10 Meta Meeting #2</div></li>
       <li><div>2021-05-09 General Session 2.5 - Voyaging 2/4 + Noticing Game</div></li>
       <li><div>2021-05-08 Informational Session</div></li>
+      <li><div>2021-05-03 Meta Meeting #1</div></li>
       <li><div>2021-05-02 General Session 2.4 - Voyaging 1/4 + Empathy Circling</div></li>
       <li><div>2021-05-01 Informational Session</div></li>
       <li><div>2021-04-25 General Session 2.3 - Boat-building part 2</div></li>
