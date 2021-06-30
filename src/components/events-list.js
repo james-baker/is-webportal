9e4a111309d05d24966102d45f6af80f2ca5c46b
James Baker <james-baker@users.noreply.github.com>
2021-06-29 22:20:45 -0700

    Update events-list.js

--- src/components/events-list.js
+++ src/components/events-list.js
@@ -28,9 +28,11 @@ export default () => (
 
     <h2>Historical Timeline</h2>
     <ul>
-      <li><div>2021-06-20 General Session 2.10 - T-Group</div></li>
+      <li><div>2021-06-27 General Session 2.12 - T-Group</div></li>
+      <li><div>2021-06-21 Meta Meeting #8</div></li>
+      <li><div>2021-06-20 General Session 2.11 - T-Group</div></li>
       <li><div>2021-06-14 Meta Meeting #7</div></li>
-      <li><div>2021-06-13 General Session 2.9 - Redesign + Planning + The Wave</div></li>
+      <li><div>2021-06-13 General Session 2.10 - Redesign + Planning + The Wave</div></li>
       <li><div>2021-06-12 Informational Session</div></li>
       <li><div>2021-06-07 Meta Meeting #6</div></li>
       <li><div>2021-06-06 General Session 2.9 - Voyage Retrospective</div></li>
@@ -52,25 +54,25 @@ export default () => (
       <li><div>2021-04-18 General Session 2.2 - Boat-building part 1</div></li>
       <li><div>2021-04-11 General Session 2.1 - Structural redesign</div></li>
       <li><div>2021-04-10 Informational Session</div></li>
-      <li><div>2021-03-27 General Session #11 - Adult Development intro part 2</div></li>
+      <li><div>2021-03-27 General Session 1.11 - Adult Development intro part 2</div></li>
       <li><div>2021-03-26 Informational Session</div></li>
-      <li><div>2021-03-21 General Session #10 - Adult Development intro part 1</div></li>
+      <li><div>2021-03-21 General Session 1.10 - Adult Development intro part 1</div></li>
       <li><div>2021-03-21 Informational Session</div></li>
-      <li><div>2021-03-14 General Session #9 - Collective Presencing</div></li>
-      <li><div>2021-03-07 General Session #8 - Authentic Relating, Hotseat</div></li>
+      <li><div>2021-03-14 General Session 1.9 - Collective Presencing</div></li>
+      <li><div>2021-03-07 General Session 1.8 - Authentic Relating, Hotseat</div></li>
       <li><div>2021-03-07 Informational Session</div></li>
-      <li><div>2021-02-28 General Session #7 - Glass Bead Game</div></li>
+      <li><div>2021-02-28 General Session 1.7 - Glass Bead Game</div></li>
       <li><div>2021-02-28 Informational Session</div></li>
-      <li><div>2021-02-21 General Session #6 - Empathy Circling</div></li>
+      <li><div>2021-02-21 General Session 1.6 - Empathy Circling</div></li>
       <li><div>2021-02-21 Informational Session</div></li>
-      <li><div>2021-02-14 General Session #5 - DEI</div></li>
-      <li><div>2021-02-07 General Session #4 - examining avoidance</div></li>
+      <li><div>2021-02-14 General Session 1.5 - DEI</div></li>
+      <li><div>2021-02-07 General Session 1.4 - examining avoidance</div></li>
       <li><div>2021-02-07 Informational Session</div></li>
-      <li><div>2021-01-31 General Session #3 - I seem, we seem</div></li>
+      <li><div>2021-01-31 General Session 1.3 - I seem, we seem</div></li>
       <li><div>2021-01-31 Informational Session</div></li>
-      <li><div>2021-01-24 General Session #2 - being with you, I notice</div></li>
+      <li><div>2021-01-24 General Session 1.2 - being with you, I notice</div></li>
       <li><div>2021-01-24 Informational Session</div></li>
-      <li><div>2021-01-17 General Session #1 - sensing and seeking</div></li>
+      <li><div>2021-01-17 General Session 1.1 - sensing and seeking</div></li>
       <li><div>2021-01-17 Informational Session</div></li>
       <li><div>2021-01-10 Informational Session</div></li>
       <li><div>2020-12-27 Informational Session</div></li>
