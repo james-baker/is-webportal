a578f8117efa57d394b66fddff36926369ec4675
James Baker <james-baker@users.noreply.github.com>
2021-07-04 19:50:27 -0700

    redraft home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { Link } from "gatsby"
 import Layout from "../components/layout"
 import CenteredColumn from "../components/centered-column"
 
@@ -7,18 +7,28 @@ const NamedDefault = ({ data }) => (
   <Layout>
     <div style={{ height: "5vh" }}></div>
     <CenteredColumn>
-      <div dangerouslySetInnerHTML={{__html: data.pitch.childMarkdownRemark.html}}></div>
+      <center>
+        PREVIEW BRANCH REDESIGN-202107<br/>
+        (Words over hero image:)<br/>
+        becoming your biggest self<br/>
+        growing with like-minded friends<br/>
+        awareness of self and system<br/>
+        integrating acceptance and intention<br/><br/><br/>
+      </center>
+      <h3>What is Intentional Society?</h3>
+
+      <p>The short answer is,
+      <blockquote>&#8220;a community for people who want to grow.&#8221;</blockquote></p>
+
+      <p>That's it - just people, being and doing together.</p>
+      <p>We are building it, 
+        learn about <Link to="/who-we-are">who we are</Link>
+        learn about <Link to="/what-we-do">what we do</Link>
+      </p>
+      <p>if and when your curiosity is piqued, the door is open for you to 
+      <Link to="/get-involved">get involved</Link>.</p>
+    
     </CenteredColumn>
   </Layout>
 )
 export default NamedDefault;
-
-export const query = graphql`
-  query {
-    pitch: file(relativePath: { eq: "md/homepage-pitch.md" }) {
-      childMarkdownRemark {
-        html
-      }
-    }
-  }
-`
