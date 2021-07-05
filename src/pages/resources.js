cba25dd2b37e2539e33d0f422f636a6e8e26d8f2
James Baker <james-baker@users.noreply.github.com>
2021-07-05 14:27:38 -0700

    bare-bones list of resources

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -8,7 +8,21 @@ const NamedDefault = ({ data }) => (
     <CenteredColumn>
       <h2>Resources</h2>
       <h3>Relational Practices</h3>
+      <h4>Authentic Relating</h4>
+      <h4>Collective Presencing</h4>
+      <h4>Empathy Circling</h4>
+      <h4>Circling</h4>
+      <h4>T-group</h4>
+      <h4>Glass Bead Game</h4>
+      <h4>Inquiry Spiraling</h4>
+      <h4>Agile Retrospectives</h4>
       <h3>Friends of Intentional Society</h3>
+      <h4>Starter Cultures</h4>
+      <h4>Microsolidarity</h4>
+      <h4>The Bridge Community</h4>
+      <h4>Enspiral</h4>
+      <h4>Sacred Ground</h4>
+      <h4>Building Belonging</h4>
     </CenteredColumn>
   </Layout>
 );
