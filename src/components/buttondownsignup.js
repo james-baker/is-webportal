d9d9a968281d1f6598d4c991bf1fcb70f20030c0
James Baker <james-baker@users.noreply.github.com>
2021-07-06 11:14:06 -0700

    why was that email+button form so wide in the first place

--- src/components/buttondownsignup.js
+++ src/components/buttondownsignup.js
@@ -4,7 +4,7 @@ import * as styles from "./buttondownsignup.module.css"
 const NamedDefault = () => <>
   <p>Want to hear about what we're doing and learning each week? 
       Enter your email here to subscribe to our progress update newsletter:</p>
-  <div style={{minWidth: "300px", maxWidth: "450px", marginBlock: "1em"}}>
+  <div style={{minWidth: "200px", maxWidth: "300px", marginBlock: "1em"}}>
     <div className={styles.popout}>
       <form
         action="https://buttondown.email/api/emails/embed-subscribe/intentionalsociety"
