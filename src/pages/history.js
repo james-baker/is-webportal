a0209c84382e55a2661e8138fbf1f82d9029cded
James Baker <james-baker@users.noreply.github.com>
2022-01-24 16:44:03 -0800

    Update history.js

--- src/pages/history.js
+++ src/pages/history.js
@@ -43,6 +43,8 @@ const NamedDefault = ({ data }) => (
 
       <Typography variant="h4">Running log of recent meetings</Typography>
       <ul>
+        <li><div>2022-01-24 Meta Meeting 5.4</div></li>
+        <li><div>2022-01-23 General Session 5.4 - Fast-CP Reflection on IFS &amp; Anniversary Celebration</div></li>
         <li><div>2022-01-22 Orientation Session</div></li>
         <li><div>2022-01-17 Governance Design Meeting</div></li>
         <li><div>2022-01-17 Meta Meeting 5.3</div></li>
