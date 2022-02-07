3fc1242f8cecf2ca2177ea3a66d227037d317146
James Baker <james-baker@users.noreply.github.com>
2022-02-07 11:18:21 -0800

    change "moved to" back to "on"

--- src/components/zoomlink-orientation.js
+++ src/components/zoomlink-orientation.js
@@ -3,6 +3,6 @@ import React from "react"
 const NamedDefault = ({ data }) => <>
   <a href="https://us02web.zoom.us/meeting/register/tZcufuGspjorEtVuebIBgnUTjev94fsw5_GF">
   Click here to sign up</a> for our next orientation video call 
-  MOVED TO <b>Saturday, February 12<sup>th</sup> at 1:00-1:55pm Pacific Standard Time (4pm Eastern, 9pm UTC)</b>.
+  on <b>Saturday, February 12<sup>th</sup> at 1:00-1:55pm Pacific Standard Time (4pm Eastern, 9pm UTC)</b>.
 </>
 export default NamedDefault;
