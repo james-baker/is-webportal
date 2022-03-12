cb49ac0457f927b7ec2c3bf640ee5c71f24fc169
James Baker <james-baker@users.noreply.github.com>
2022-03-12 14:29:07 -0800

    Update zoomlink-orientation.js
    with daylight savings transition

--- src/components/zoomlink-orientation.js
+++ src/components/zoomlink-orientation.js
@@ -1,8 +1,8 @@
 import React from "react"
 
 const NamedDefault = ({ data }) => <>
-  <a href="https://us02web.zoom.us/meeting/register/tZAvf-6vqz8uHdFFUDDZp1gQ63gscZjFqG-J">
+  <a href="https://us02web.zoom.us/meeting/register/tZEldOGorj4iG9IkHk5v9UzxK4zFkmcd7n0f">
   Click here to sign up</a> for our next orientation video call 
-  on <b>Saturday, March 12<sup>th</sup> at 1:00-1:55pm Pacific Standard Time (4pm Eastern, 9pm UTC)</b>.
+  on <b>Saturday, March 19<sup>th</sup> at 1:00-1:55pm Pacific Daylight Time (4pm Eastern, 8pm UTC)</b>.
 </>
 export default NamedDefault;
