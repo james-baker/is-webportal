648c7f7963ec54b170093205b6543260ec27a0c5
James Baker <james-baker@users.noreply.github.com>
2022-08-13 13:50:08 -0700

    next orientation call

--- src/components/zoomlink-orientation.js
+++ src/components/zoomlink-orientation.js
@@ -1,8 +1,8 @@
 import React from "react"
 
 const NamedDefault = ({ data }) => <>
-  <a href="https://us02web.zoom.us/meeting/register/tZMvce6qqz0oE9yj-EYwW82e_qFkTaRNhDBP">
+  <a href="https://us02web.zoom.us/meeting/register/tZApduGurj8vHNPwS1HPN2Uq0Ull6pgMomtI">
   Click here to register</a> for our next orientation video call 
-  on <b>Saturday, August 13<sup>th</sup> at 1:00-1:55pm Pacific Daylight Time (4pm Eastern, 8pm UTC)</b>.
+  on <b>Saturday, August 27<sup>th</sup> at 1:00-1:55pm Pacific Daylight Time (4pm Eastern, 8pm UTC)</b>.
 </>
 export default NamedDefault;
--- src/pages/history.js
+++ src/pages/history.js
@@ -43,6 +43,7 @@ const NamedDefault = ({ data }) => (
 
       <Typography variant="h5">Season 7 - Q3 2022</Typography>
       <ul>
+        <li><div>2022-08-13 Orientation Session</div></li>
         <li><div>2022-08-08 Meta Meeting 7.5</div></li>
         <li><div>2022-08-07 General Session 7.5 - Open Session w/ Seishin</div></li>
         <li><div>2022-08-01 Meta Meeting 7.4</div></li>
