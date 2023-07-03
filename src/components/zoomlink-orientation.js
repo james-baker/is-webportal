d6dba56483c16b5f304018d4c0f5c911a03c41a8
James Baker <james-baker@users.noreply.github.com>
2023-07-02 21:55:12 -0700

    Update zoomlink-orientation.js

--- src/components/zoomlink-orientation.js
+++ src/components/zoomlink-orientation.js
@@ -1,8 +1,8 @@
 import React from "react"
 
 const NamedDefault = ({ data }) => <>
-  <a href="https://us02web.zoom.us/meeting/register/tZwpcumrqzIvHdfj5NtkVf35wyQBBVBxtmva">
+  <a href="https://us02web.zoom.us/meeting/register/tZIsdO-qqzgsGtIVHVZ3xLn2CUPsvGlm89bm">
   Click here to register</a> for our next orientation video call 
-  on <b>Saturday, July 1<sup>st</sup> at 1:00-1:55pm Pacific Time (4pm Eastern, 8pm UTC)</b>.
+  on <b>Saturday, July 8<sup>th</sup> at 1:00-1:55pm Pacific Time (4pm Eastern, 8pm UTC)</b>.
 </>
 export default NamedDefault;
