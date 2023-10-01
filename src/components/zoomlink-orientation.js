8c303d7e9976b508dd18a3e837c0c23ca09c8888
James Baker <james-baker@users.noreply.github.com>
2023-10-01 12:32:51 -0700

    typo fix

--- src/components/zoomlink-orientation.js
+++ src/components/zoomlink-orientation.js
@@ -5,7 +5,7 @@ const NamedDefault = ({ data }) => <>
   Click here to register</a> for our next orientation video call 
   on <b>Saturday, October 7<sup>th</sup> at 1:00-1:55pm Pacific Time (4pm Eastern, 8pm UTC)</b>. 
   This is an extra-special edition just prior to our
-  upcoming <a href="https://buttondown.email/intentionalsociety/archive/intentional-society-season-of-change/">
-  Season of Change"</a> finishing out 2023.
+  upcoming "<a href="https://buttondown.email/intentionalsociety/archive/intentional-society-season-of-change/">
+  Season of Change</a>" finishing out 2023.
 </>
 export default NamedDefault;
