e29741963086f4c654bd4f538c729127ddcca3fc
James Baker <james-baker@users.noreply.github.com>
2023-10-01 14:13:25 -0700

    short and sweet landing page update

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,28 +16,33 @@ const NamedDefault = ({ data }) => <>
     <div style={{  minWidth: "350px", maxWidth: "400px", width: "fit-content", margin: "0 auto" }}>
       <Typography variant="h5" style={{ color: "#FFFFFF"}}>
         <p style={{marginLeft:"1vw"}}>become your biggest self</p>
-        <p style={{marginLeft:"4vw"}}>grow with like-minded friends</p>
-        <p style={{marginLeft:"-1vw"}}>awareness and perspective</p>
-        <p style={{marginLeft:"2vw"}}>accepting and integrating</p><br/>&nbsp;<br/>&nbsp;<br/>&nbsp;<br/>&nbsp;
+        <p style={{marginLeft:"4vw"}}>awareness, acceptance, integrity</p>
+        <p style={{marginLeft:"-1vw"}}>growing a collective</p>
+        <p style={{marginLeft:"2vw"}}>in service of life</p><br/>&nbsp;<br/>&nbsp;<br/>&nbsp;<br/>&nbsp;
       </Typography>
     </div>
     <div style={{margin: "-10vh auto 0", minWidth: "300px", maxWidth: "960px", padding: "0 50px"}}>
-      <p>Intentional Society is a community for people who want to grow.</p>
+      <p>Intentional Society is people bringing awareness to intention in 
+         becoming, belonging, and doing together.<br/>
+      It is a place to grow and develop oneself in a warm, supportive community.
+      It is a future society that supports the ways we want to inhabit our best selves.
+      It is a living probe into self-transforming collective culture.</p>
       <IsHr />
-      <p>We meet together to become our best and biggest selves - to practice being 
+      <p>We meet frequently over video to become our best and biggest selves — to practice being 
         the people we want to be, more consistently, with more ease and freedom. In  
         doing so, we also then increase our capacity to love and serve both ourselves and humanity.</p>
 
-      <p>Intentional Society is also simply people, in process, 
-        being and doing together, as we continue to both create and discover the fullness of what it will become. 
-        Read on to learn more about <Link to="/who-we-are">who we 
-        are</Link> and <Link to="/what-we-do">what we do</Link>, and how 
+      <p>Intentional Society is currently (late 2023) undergoing metamorphosis!
+        In this season of change, our website has not yet been updated to reflect 
+        the uncertainty and possibility of how we are shifting and expanding. 
+        You can still read previous versions of <Link to="/who-we-are">who we 
+        are</Link> and <Link to="/what-we-do">what we do</Link>, and find how 
         to <Link to="/get-involved">get involved</Link> if and when you feel so called.
       </p>
       <IsHr />
-      <p>We've <Link to="/history">held 100+ gatherings</Link> in our first few years, and continue our 
-        exploration and expansion week by week. 
-       Our 11<sup>th</sup> quarterly season runs July through September (Q3) of 2023.<br />
+      <p>We've been exploring and relating together since January 2021, 
+        with <Link to="/history">more than 130 weekly sessions</Link> under our belts. 
+       Our 12<sup>th</sup> quarterly season runs October through December of 2023.<br />
       <ZoomlinkOrientation /></p>
       <IsHr />
       <p>Want to follow along with what we're doing and learning? 
