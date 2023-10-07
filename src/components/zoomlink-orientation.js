ceada7293c33bc4dbda923c7a03fdcf7454fa5a1
James Baker <james-baker@users.noreply.github.com>
2023-10-07 15:32:43 -0700

    Update zoomlink-orientation.js

--- src/components/zoomlink-orientation.js
+++ src/components/zoomlink-orientation.js
@@ -1,11 +1,8 @@
 import React from "react"
 
 const NamedDefault = ({ data }) => <>
-  <a href="https://us02web.zoom.us/meeting/register/tZckdO-qrDIjGdNv2YdkXcxvyqdjCB4sjeJj">
+  <a href="https://us02web.zoom.us/meeting/register/tZYpce6hrjgsGdOa8LelI3F5JVGvrIBrHloU">
   Click here to register</a> for our next orientation video call 
-  on <b>Saturday, October 7<sup>th</sup> at 1:00-1:55pm Pacific Time (4pm Eastern, 8pm UTC)</b>. 
-  This is an extra-special edition just prior to our
-  upcoming "<a href="https://buttondown.email/intentionalsociety/archive/intentional-society-season-of-change/">
-  Season of Change</a>" finishing out 2023.
+  on <b>Friday, October 13<sup>th</sup> at 12:00-12:55pm Pacific Time (3pm Eastern, 7pm UTC)</b>. 
 </>
 export default NamedDefault;
