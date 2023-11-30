e722ca3eaaaf8f4a840413e7d9c8661fb2aa7e6f
James Baker <james-baker@users.noreply.github.com>
2023-11-30 14:46:53 -0800

    oops that's a Saturday

--- src/components/zoomlink-orientation.js
+++ src/components/zoomlink-orientation.js
@@ -3,6 +3,6 @@ import React from "react"
 const NamedDefault = ({ data }) => <>
   <a href="https://us02web.zoom.us/meeting/register/tZwudO-vpj4pHNUun_vVgvKG6t-Whtyg6Ovu">
   Click here to register</a> for our next orientation video call 
-  on <b>Friday, December 16<sup>th</sup> at 1:00-1:55pm Pacific Time (4pm Eastern, 9pm UTC)</b>. 
+  on <b>Saturday, December 16<sup>th</sup> at 1:00-1:55pm Pacific Time (4pm Eastern, 9pm UTC)</b>. 
 </>
 export default NamedDefault;
