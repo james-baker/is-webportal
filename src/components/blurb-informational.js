147aa122003b13192d970762c3bcda4dfde65ba6
James Baker <james-baker@users.noreply.github.com>
2024-01-06 14:00:51 -0800

    Update blurb-informational.js

--- src/components/blurb-informational.js
+++ src/components/blurb-informational.js
@@ -1,8 +1,10 @@
 import React from "react"
 
 const NamedDefault = ({ data }) => <>
-  <a href="https://us02web.zoom.us/meeting/register/tZwkfuispzsjGNTtPOkU3WymYKEVBozocwoK">
+  <a href="">
   Click here to register</a> for our next informational video call 
-  on <b>Saturday, January 6<sup>th</sup> at 1:00-1:55pm Pacific Time (4pm Eastern, 9pm UTC)</b>. 
+  on &lt;currenlty unscheduled&gt;.
 </>
 export default NamedDefault;
+
+//<b>Saturday, January 6<sup>th</sup> at 1:00-1:55pm Pacific Time (4pm Eastern, 9pm UTC)</b>
