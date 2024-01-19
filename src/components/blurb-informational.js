61c92fedc473a511a20fdbcd54dc6b9a95625f6e
James Baker <james-baker@users.noreply.github.com>
2024-01-19 14:08:23 -0800

    fix info session to registration link too

--- src/components/blurb-informational.js
+++ src/components/blurb-informational.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 const NamedDefault = ({ data }) => <>
-  <a href="https://us02web.zoom.us/j/82104925350?pwd=S1FYSmVLajVwYmdVSlFqN2hjdzNEZz09">
+  <a href="https://us02web.zoom.us/meeting/register/tZEpc-Cqpz0qGtapPrliK9a3n1iPRHiyehWz">
   Click here to register</a> for our next informational video call 
   on <b>Friday, January 26<sup>th</sup> at 1:00-1:55pm Pacific Time (4pm Eastern, 9pm UTC)</b>.
 </>
