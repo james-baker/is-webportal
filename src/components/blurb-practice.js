39b17b6a32f8e091697db5ab148acc8599422ea5
James Baker <james-baker@users.noreply.github.com>
2024-01-31 14:43:14 -0800

    Update blurb-practice.js

--- src/components/blurb-practice.js
+++ src/components/blurb-practice.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 const NamedDefault = ({ data }) => <>
-  The next session in our practice series is <b>Sunday, January 21<sup>st</sup> at 
+  The next session in our practice series is <b>Sunday, February<sup>th</sup> at 
   1:00-2:30pm Pacific Time (4pm Eastern, 9pm UTC)</b>. <a 
   href="https://forms.gle/qvX1Fz9c4Wj3F2wAA">Click here for access to the meeting link.</a>
 </>
