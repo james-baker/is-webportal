d15645dd0909d06470c361285d5e41bcc39e9859
James Baker <james-baker@users.noreply.github.com>
2024-03-14 20:30:26 -0700

    Update blurb-practice.js

--- src/components/blurb-practice.js
+++ src/components/blurb-practice.js
@@ -1,8 +1,8 @@
 import React from "react"
 
 const NamedDefault = ({ data }) => <>
-  The next session in our practice series is <b>Sunday, March 10<sup>th</sup> at 
-  1:00-2:30pm Pacific Time (4pm Eastern, 8pm UTC)</b>. Note, USA Daylight Savings shift takes effect that day! <a 
+  The next (and final) session in our practice series is <b>Sunday, March 17<sup>th</sup> at 
+  1:00-2:30pm Pacific Time (4pm Eastern, 8pm UTC)</b>. <a 
   href="https://forms.gle/qvX1Fz9c4Wj3F2wAA">Click here for access to the meeting link.</a>
 </>
 export default NamedDefault;
