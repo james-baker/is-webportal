7a6e297dc16e1c0c22b07547b74278855a1bdfa2
James Baker <james-baker@users.noreply.github.com>
2024-04-03 20:15:13 -0700

    Update blurb-practice.js

--- src/components/blurb-practice.js
+++ src/components/blurb-practice.js
@@ -1,8 +1,9 @@
 import React from "react"
 
 const NamedDefault = ({ data }) => <>
-  The next (and final) session in our practice series is <b>Sunday, March 17<sup>th</sup> at 
+  The first session in our Exploratory Practice Series is <b>Saturday, April 13<sup>th</sup> at 
   1:00-2:30pm Pacific Time (4pm Eastern, 8pm UTC)</b>. <a 
-  href="https://forms.gle/qvX1Fz9c4Wj3F2wAA">Click here for access to the meeting link.</a>
+  href="https://docs.google.com/forms/d/e/1FAIpQLScft8sEI3FuL-faqRU7w8Hp5gTrj0zePg6-bhKJM_gwXaVqVg/viewform">Click 
+  here for access to the meeting link.</a>
 </>
 export default NamedDefault;
