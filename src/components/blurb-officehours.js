23a480379d55c3a4284c4de4a85038b05be9bf04
James Baker <james-baker@users.noreply.github.com>
2024-04-25 12:49:51 -0700

    Update blurb-officehours.js

--- src/components/blurb-officehours.js
+++ src/components/blurb-officehours.js
@@ -1,8 +1,8 @@
 import React from "react"
 
 const NamedDefault = ({ data }) => <>
-  <a href="https://us02web.zoom.us/meeting/register/tZcpdumvrTkiH9Sa9SRwfGEY3_uw5fe1F8H1">
+  <a href="https://us02web.zoom.us/meeting/register/tZMlcuqgqDIiH9cGNK3mnQ1Vy2XxG-g4sk9g">
   Click here to register</a> for our next Office Hours video call 
-  on <b>Friday, April 19<sup>th</sup> at 1:00-1:55pm Pacific Time (4pm Eastern, 9pm UTC)</b>.
+  on <b>Friday, May 3<sup>rd</sup> at 1:00-1:55pm Pacific Time (4pm Eastern, 9pm UTC)</b>.
 </>
 export default NamedDefault;
