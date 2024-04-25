78750699b679d7d8ccbe25e55404e619ebdc3b78
James Baker <james-baker@users.noreply.github.com>
2024-04-25 14:41:08 -0700

    Update blurb-practice.js

--- src/components/blurb-practice.js
+++ src/components/blurb-practice.js
@@ -1,8 +1,8 @@
 import React from "react"
 
 const NamedDefault = ({ data }) => <>
-  The first session in our Exploratory Practice Series is <b>Saturday, April 13<sup>th</sup> at 
-  1:00-2:30pm Pacific Time (4pm Eastern, 8pm UTC)</b>. <a 
+  The next session in our Exploratory Practice Series is <b>Saturday, April 27<sup>th</sup> at 
+  1:00-3:00pm Pacific Time (4pm Eastern, 8pm UTC)</b>. <a 
   href="https://docs.google.com/forms/d/e/1FAIpQLScft8sEI3FuL-faqRU7w8Hp5gTrj0zePg6-bhKJM_gwXaVqVg/viewform">Click 
   here for access to the meeting link.</a>
 </>
