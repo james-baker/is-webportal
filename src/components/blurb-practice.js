347d4fcb37b688cf0fbb0e7483b146ed0bfa4d4e
James Baker <james-baker@users.noreply.github.com>
2024-06-06 17:09:28 -0700

    Update blurb-practice.js

--- src/components/blurb-practice.js
+++ src/components/blurb-practice.js
@@ -1,8 +1,9 @@
 import React from "react"
 
 const NamedDefault = ({ data }) => <>
-  The next session in our Exploratory Practice Series is <b>Saturday, May 25<sup>th</sup> at 
-  1:00-3:00pm Pacific Time (4pm Eastern, 8pm UTC)</b>. <a 
+  The next session in our Exploratory Practice Series, <b>Mettā Meditation</b> with Tasshin Fogleman,
+  is <b>Saturday, June 8<sup>th</sup> at 
+  1:00-2:30pm Pacific Time (4pm Eastern, 8pm UTC)</b>. <a 
   href="https://docs.google.com/forms/d/e/1FAIpQLScft8sEI3FuL-faqRU7w8Hp5gTrj0zePg6-bhKJM_gwXaVqVg/viewform">Click 
   here for access to the meeting link.</a>
 </>
