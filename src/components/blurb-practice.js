a96785d737f4928f62a78e6e4a2da7655764b022
James Baker <james-baker@users.noreply.github.com>
2024-07-02 13:41:58 -0700

    Update blurb-practice.js

--- src/components/blurb-practice.js
+++ src/components/blurb-practice.js
@@ -1,10 +1,10 @@
 import React from "react"
 
 const NamedDefault = ({ data }) => <>
-  The next session in our Exploratory Practice Series, <b>Mettā Meditation</b> with Tasshin Fogleman,
-  is <b>Saturday, June 8<sup>th</sup> at 
+  Our Q2 Exploratory Practice Series is complete. Q3 series dates TBD.
+  {/* is <b>Saturday, June 8<sup>th</sup> at 
   1:00-2:30pm Pacific Time (4pm Eastern, 8pm UTC)</b>. <a 
   href="https://docs.google.com/forms/d/e/1FAIpQLScft8sEI3FuL-faqRU7w8Hp5gTrj0zePg6-bhKJM_gwXaVqVg/viewform">Click 
-  here for access to the meeting link.</a>
+  here for access to the meeting link.</a> */}
 </>
 export default NamedDefault;
