dd795e61e91df7a7148cc21011d78f79f7ff7cf0
James Baker <james-baker@users.noreply.github.com>
2024-07-02 14:49:09 -0700

    typofix

--- src/components/blurb-officehours.js
+++ src/components/blurb-officehours.js
@@ -3,6 +3,6 @@ import React from "react"
 const NamedDefault = ({ data }) => <>
   <a href="https://us02web.zoom.us/meeting/register/tZwkdOytpjwuGtU3gCDcGfbAVnvq8VPj2me1">
   Click here to register</a> for our next Office Hours video call 
-  on <b>Saturda, July 6<sup>th</sup> at 1:00-1:55pm Pacific Time (4pm Eastern, 9pm UTC)</b>.
+  on <b>Saturday, July 6<sup>th</sup> at 1:00-1:55pm Pacific Time (4pm Eastern, 9pm UTC)</b>.
 </>
 export default NamedDefault;
