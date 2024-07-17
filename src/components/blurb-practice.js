1b0b939258aeee2f680b60f44aed215f9e70a96b
James Baker <james-baker@users.noreply.github.com>
2024-07-17 11:03:25 -0700

    Update blurb-practice.js

--- src/components/blurb-practice.js
+++ src/components/blurb-practice.js
@@ -1,10 +1,8 @@
 import React from "react"
 
 const NamedDefault = ({ data }) => <>
-  Our Q2 Exploratory Practice Series is complete. Q3 series dates TBD.
-  {/* is <b>Saturday, June 8<sup>th</sup> at 
-  1:00-2:30pm Pacific Time (4pm Eastern, 8pm UTC)</b>. <a 
-  href="https://docs.google.com/forms/d/e/1FAIpQLScft8sEI3FuL-faqRU7w8Hp5gTrj0zePg6-bhKJM_gwXaVqVg/viewform">Click 
-  here for access to the meeting link.</a> */}
+  The next session of our Q3 Connection Lab series 
+   is <b>Saturday, July 27<sup>th</sup> at 
+  1:00-2:30pm Pacific Time (4pm Eastern, 8pm UTC)</b>. Link will be emailed to the newsletter and posted here Thursday the 25<sup>th</sup>.
 </>
 export default NamedDefault;
