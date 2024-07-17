ee0cc868d5a72756fe0fded8e5ea267d2b96d654
James Baker <james-baker@users.noreply.github.com>
2024-07-17 11:10:39 -0700

    stale UTC time

--- src/components/blurb-officehours.js
+++ src/components/blurb-officehours.js
@@ -3,6 +3,6 @@ import React from "react"
 const NamedDefault = ({ data }) => <>
   <a href="https://us02web.zoom.us/meeting/register/tZcvcOmtpjwoH9bDxEm9J4yWzWTCpcV2-3t5">
   Click here to register</a> for our next Office Hours video call 
-  on <b>Friday, August 2<sup>nd</sup> at 1:00-1:55pm Pacific Time (4pm Eastern, 9pm UTC)</b>.
+  on <b>Friday, August 2<sup>nd</sup> at 1:00-1:55pm Pacific Time (4pm Eastern, 8pm UTC)</b>.
 </>
 export default NamedDefault;
