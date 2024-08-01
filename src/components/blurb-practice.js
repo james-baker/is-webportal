880b0626597cc953f5816adccdc7ce716838530f
James Baker <james-baker@users.noreply.github.com>
2024-08-01 14:56:00 -0700

    Update blurb-practice.js

--- src/components/blurb-practice.js
+++ src/components/blurb-practice.js
@@ -2,13 +2,12 @@ import React from "react"
 
 const NamedDefault = ({ data }) => <>
   The next upcoming session of our Practice Dojo Connection Lab series 
-   is <b>Saturday, July 27<sup>th</sup> at 
+   is <b>Saturday, August 17<sup>th</sup> at 
   1:00-2:30pm Pacific Time (4pm Eastern, 8pm UTC)</b>. <a 
-  href="https://us02web.zoom.us/meeting/register/tZMkdOiuqjwsGde0BujmYnnSP48Lcqsg-RaK">Click 
+  href="https://us02web.zoom.us/meeting/register/tZMlfu2gqTsvE927JglIZKGqfJAZZORI_IBa">Click 
   here to register and receive the meeting link.</a>
   <br /><br />
   You can also register for 
-  the <a href="https://us02web.zoom.us/meeting/register/tZMlfu2gqTsvE927JglIZKGqfJAZZORI_IBa">August 17th</a> and <a 
-  href="https://us02web.zoom.us/meeting/register/tZEvc-2qrDoqHNAFva8GMK_UTfaMrItyBsfB">September 7th</a> sessions.
+  the <a href="https://us02web.zoom.us/meeting/register/tZEvc-2qrDoqHNAFva8GMK_UTfaMrItyBsfB">September 7th</a> session.
 </>
 export default NamedDefault;
