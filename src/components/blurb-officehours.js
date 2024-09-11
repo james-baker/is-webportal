6bafa0201bc55dd8c3ba37e264e3ea1b98873206
James Baker <james-baker@users.noreply.github.com>
2024-09-11 13:39:40 -0700

    the 13th is a Friday

--- src/components/blurb-officehours.js
+++ src/components/blurb-officehours.js
@@ -3,6 +3,6 @@ import React from "react"
 const NamedDefault = ({ data }) => <>
   <a href="https://us02web.zoom.us/meeting/register/tZAqc-uurTksGdeHZWtl68rggZmJEJwOEV1I">
   Click here to register</a> for our next Office Hours video call 
-  on <b>Saturday, September 13<sup>th</sup> at 1:00-1:55pm Pacific Time (4pm Eastern, 8pm UTC)</b>.
+  on <b>Friday, September 13<sup>th</sup> at 1:00-1:55pm Pacific Time (4pm Eastern, 8pm UTC)</b>.
 </>
 export default NamedDefault;
