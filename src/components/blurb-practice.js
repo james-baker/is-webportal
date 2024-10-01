0f9fd01a09bcd99f177e5db2d9ff4bcabcabc365
James Baker <james-baker@users.noreply.github.com>
2024-10-01 14:28:57 -0700

    Update blurb-practice.js

--- src/components/blurb-practice.js
+++ src/components/blurb-practice.js
@@ -1,11 +1,9 @@
 import React from "react"
 
 const NamedDefault = ({ data }) => <>
-  The next upcoming session of our Practice Dojo Q4 2024 series 
-   is TBD.
-{/*    <b>Saturday, September 7<sup>th</sup> at 
-  1:00-2:30pm Pacific Time (4pm Eastern, 8pm UTC)</b>. <a 
-  href="https://us02web.zoom.us/meeting/register/tZEvc-2qrDoqHNAFva8GMK_UTfaMrItyBsfB">Click 
-  here to register and receive the meeting link.</a> */}
+  The next upcoming Practice Dojo session is on <b>Saturday, October 19<sup>th</sup> at 
+  1:00-2:30pm Pacific Time (4pm Eastern, 8pm UTC)</b>. TODO, LINK COMING SOON <a 
+  href="">Click 
+  here to register and receive the meeting link.</a>
 </>
 export default NamedDefault;
