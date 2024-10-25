14c229d4af66a31dfe70e131a96bbd08a14c3ae4
James Baker <james-baker@users.noreply.github.com>
2024-10-25 11:50:26 -0700

    Update blurb-practice.js

--- src/components/blurb-practice.js
+++ src/components/blurb-practice.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 const NamedDefault = ({ data }) => <>
-  The next upcoming "Being With It All" Practice Dojo session is on <b>Saturday, October 19<sup>th</sup> at 
+  The next upcoming "Being With It All" Practice Dojo session is on <b>Saturday, November 2<sup>nd</sup> at 
   1:00-2:30pm Pacific Time (4pm Eastern, 8pm UTC)</b>. <a 
   href="https://us02web.zoom.us/meeting/register/tZIvdOCtpj4qG9XxuaHMOq_7eGs_6dPCwhae">Click 
   here to register and receive the meeting link.</a>
