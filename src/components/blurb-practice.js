748a9b947ad05be0a9fb3b976a4be98d5aae1713
James Baker <james-baker@users.noreply.github.com>
2024-12-19 16:00:48 -0800

    Update blurb-practice.js

--- src/components/blurb-practice.js
+++ src/components/blurb-practice.js
@@ -1,9 +1,10 @@
 import React from "react"
 
 const NamedDefault = ({ data }) => <>
-  The next upcoming "Being With It All" Practice Dojo session is on <b>Saturday, December 14<sup>th</sup> at 
-  1:00-2:30pm Pacific Time (4pm Eastern, 8pm UTC)</b>. <a 
+  The next Practice Dojo series and sessions are TBD. Sessions are commonly on <b>Saturdays at 
+  1:00-2:30pm Pacific Time (4pm Eastern, 9pm UTC)</b>. 
+  {/* <a 
   href="https://us02web.zoom.us/meeting/register/tZIvdOCtpj4qG9XxuaHMOq_7eGs_6dPCwhae">Click 
-  here to register and receive the meeting link.</a>
+  here to register and receive the meeting link.</a> */}
 </>
 export default NamedDefault;
