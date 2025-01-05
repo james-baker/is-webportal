bbaec3e30cae775b3066cad1babd302a451d4a7b
James Baker <james-baker@users.noreply.github.com>
2025-01-04 21:12:51 -0800

    update home page prepping for S17

--- src/components/blurb-practice.js
+++ src/components/blurb-practice.js
@@ -1,8 +1,9 @@
 import React from "react"
 
 const NamedDefault = ({ data }) => <>
-  The next Practice Dojo series and sessions are TBD. Sessions are commonly on <b>Saturdays at 
-  1:00-2:30pm Pacific Time (4pm Eastern, 9pm UTC)</b>. 
+  The topic and timing of the Q1 Practice Dojo series are TBD and will be announced by mid-January. 
+  Sessions are usually on <b>Saturdays from 
+  1:00 to 2:30pm Pacific Time (4pm Eastern, 9pm UTC)</b>. 
   {/* <a 
   href="https://us02web.zoom.us/meeting/register/tZIvdOCtpj4qG9XxuaHMOq_7eGs_6dPCwhae">Click 
   here to register and receive the meeting link.</a> */}
--- src/pages/index.js
+++ src/pages/index.js
@@ -39,41 +39,20 @@ const NamedDefault = ({ data }) => <>
       (World:) &hellip;a future society that better supports the thriving of humans and humanity.</p>
       
       <IsHr />
-      <a className={GlobalCSS.anchorOffset} id="s14"></a>
-      <Typography variant="h3">Happening now: IS Season 16 (Q4 2024)</Typography>
+      <a className={GlobalCSS.anchorOffset} id="s17"></a>
+      <Typography variant="h3">Happening now: IS Season 17</Typography>
 
-      <p>Our 16<sup>th</sup> seasonal/quarterly cycle runs October through December 2024. Intentional 
+      <p>Year five of Intentional Society begins with our  17<sup>th</sup> seasonal/quarterly cycle,
+        running January through March of 2025. Intentional 
         Society currently has three main spaces/places with varying levels of openness and commitment. 
         Whoever and wherever you are, you are invited to draw closer in connection with the people 
         and purposes of IS.</p>
 
-      <Typography variant="h4">Practice Dojo (public offerings)</Typography>
-
-      <Typography variant="h5">Being With It All</Typography>
-
-      <p>There's a lot going on in the world and in our lives! How do you want to be, when tough news surprises you?</p>
-
-      <p>We can find choice and freedom in the space between stimulus and response. In this series, 
-        we will practice a way-of-being that welcomes reality as it is without giving up our care or 
-        agency. We'll face some challenging topics like war/violence, elections/politics, relationship 
-        stress, etc and support each other in becoming better able to Be With It All - expanding our 
-        capacity to hold grief and gratitude, acceptance and desire, powerlessness and agency, without 
-        losing ourselves in the torrent of stimulus in modern life.</p>
-
-      <p>The series is now complete!</p>
-      
-      <ul>
-        <li><s>October 19th</s></li>
-        <li><s>November 2nd</s></li>
-        <li><s>November 16th</s></li>
-        <li><s>November 30th</s></li>
-        <li><s>December 14th</s></li>
-      </ul>
-      <p>All sessions are on Saturdays starting at 1pm Pacific for 90 minutes. </p>
+      <Typography variant="h4">IS Practice Dojo (public offerings)</Typography>
 
       <p><BlurbPractice /></p>
 
-      <Typography variant="h4">IS Community (members collective)</Typography>
+      <Typography variant="h4">IS Community Hub (members collective)</Typography>
 
       <p>Community members gather on Sundays for weekly calls, and/or support one another in relational "pods", 
         while forming and deepening relationships with other community members. We play together in a relational 
@@ -85,21 +64,22 @@ const NamedDefault = ({ data }) => <>
         light application-and-agreements membrane that defines membership exists to let us 
         create openness and safety in our private cozyweb and manage our private data sharing. 
         You're invited to attend an <Link to="/get-involved#office-hours">Office Hours</Link> call 
-        for orientation purposes, yet if you've been to at least one Practice Dojo session previously, y
-        ou probably have enough of the vibe to dive in if you prefer.</p>
+        for orientation purposes, yet if you've been to at least one Practice Dojo session previously,
+        you probably have enough of the vibe to dive in if you prefer.</p>
 
       <Typography variant="h4">Intentional Ventures (private experiment)</Typography>
 
       <p>We unlock successful economics for meaningful livelihoods through relational support.</p>
 
-      <p>After two quarters in incubation with the founding team of ten, IV has established 
-        key structures and principles, and intends to invite new members from the waitlist in Q4. 
-        To be added to the waitlist, come chat about it at <Link to="/get-involved#office-hours">Office Hours</Link>.</p>
+      <p>After three quarters with its founding team, IV has made its first revenue, established 
+        key structures and principles, and added its first new participants. 
+        To be added to the waitlist, come chat about it at <Link to="/get-involved#office-hours">Office Hours</Link> or 
+        talk to any participant.</p>
 
       <br />
       <IsHr />
-      <p>Since Jan 2021 we've been exploring and relating together weekly, 
-        with <Link to="/history">more than 150 sessions</Link> under our belts.</p>
+      <p>For four years now we've been exploring and relating together weekly, 
+        with <Link to="/history">more than 200 sessions</Link> under our belts.</p>
       <p>
         You can read more about <Link to="/who-we-are">who we 
         are</Link> and <Link to="/what-we-do">what we do</Link>, and find a few 
